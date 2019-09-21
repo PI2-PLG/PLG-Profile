08a814599579be1175478b748eb177be81337f7c
tayh <tayhcarvalho@gmail.com>
2019-09-21 08:36:45 -0300

    Setting up postgres database

--- user_profile/settings.py
+++ user_profile/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'c5ycgl8)&h8@df#3ndlnbf6e_1a0vwh6=c)a-@zcbbsmdc1f&!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'user_profile.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'profiledb',
+        'PORT': 5432,
     }
 }
 
